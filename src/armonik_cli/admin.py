189132f534e94e22b3bc2abcf48964e3b6c1a816
Nicolas Gruel <nicolas.gruel@gmail.com>
2023-11-26 20:11:54 +0100

    refactor: clean some code and add comments

--- src/armonik_cli/admin.py
+++ src/armonik_cli/admin.py
@@ -1,11 +1,15 @@
+# Faire passer un linter, mypy et bandit
+# mettre en place un precommit
+
 import argparse
 import grpc
 from armonik.client.sessions import ArmoniKSessions, SessionFieldFilter
 from armonik.client.tasks import ArmoniKTasks, TaskFieldFilter
-from armonik.common.enumwrapper import TASK_STATUS_ERROR, TASK_STATUS_CREATING , SESSION_STATUS_RUNNING, SESSION_STATUS_CANCELLED, SESSION_STATUS_UNSPECIFIED
+from armonik.common.enumwrapper import TASK_STATUS_ERROR, TASK_STATUS_CREATING, \
+        SESSION_STATUS_RUNNING, SESSION_STATUS_CANCELLED
 from armonik.common.filter import Filter
 
-def create_channel(endpoint: str,  ca: str = None, key: str = None, cert: str = None) -> grpc.Channel:
+def create_channel(endpoint: str,  ca: str, cert: str, key: str) -> grpc.Channel:
     """
     Create a gRPC channel for communication with the ArmoniK control plane
 
@@ -18,23 +22,24 @@ def create_channel(endpoint: str,  ca: str = None, key: str = None, cert: str = 
     Returns:
         grpc.Channel: gRPC channel for communication
     """
-    if ca != None:
-            with open(ca, 'rb') as ca_file:
-                ca_data = ca_file.read()
-            if cert != None and key!= None :
-                with open(cert, 'rb') as cert_file, open(key, 'rb') as key_file:
-                    key_data = key_file.read()
-                    cert_data = cert_file.read()
-                credentials = grpc.ssl_channel_credentials(ca_data, key_data, cert_data)
-            else:
-                credentials = grpc.ssl_channel_credentials(ca_data)
-            return grpc.secure_channel(endpoint, credentials)
+    if ca:
+        with open(ca, 'rb') as ca_file:
+            ca_data = ca_file.read()
+        if cert and key:
+            with open(cert, 'rb') as cert_file, open(key, 'rb') as key_file:
+                key_data = key_file.read()
+                cert_data = cert_file.read()
+            credentials = grpc.ssl_channel_credentials(ca_data, key_data, cert_data)
+        else:
+            credentials = grpc.ssl_channel_credentials(ca_data)
+        return grpc.secure_channel(endpoint, credentials)
     else:
         return grpc.insecure_channel(endpoint)
 
+# Je ne sais pas si la fonction précédente ne devrait pas être un try/catch avec par défaut le secure (voir en faisant une gestion des erreurs tel que j'ai vu dans l'article (a tester)
 
-
-def list_sessions(client: ArmoniKSessions, session_filter: Filter):
+    
+def list_sessions(client: ArmoniKSessions, session_filter: Filter) -> None:
     """
     List sessions with filter options
 
@@ -45,16 +50,17 @@ def list_sessions(client: ArmoniKSessions, session_filter: Filter):
     page = 0
     sessions = client.list_sessions(session_filter, page=page)
     
-    while len(sessions[1]) > 0:
+    while sessions[0]:
         for session in sessions[1]:
             print(f'Session ID: {session.session_id}')
         page += 1
         sessions = client.list_sessions(session_filter, page=page)
-
+    
+    # est-ce que nombre contient l'integralité des sessions ou uniquement celle de la dernière page? 
     print(f'\nNumber of sessions: {sessions[0]}\n')
 
 
-def cancel_sessions(client: ArmoniKSessions, sessions: list):
+def cancel_sessions(client: ArmoniKSessions, sessions: list) -> None:
     """
     Cancel sessions with a list of session IDs or all sessions running
 
@@ -65,14 +71,14 @@ def cancel_sessions(client: ArmoniKSessions, sessions: list):
     for session_id in sessions:
         try:
             client.cancel_session(session_id)
-            print(f"Session {session_id} canceled successfully")
+            print(f"Session {session_id} canceled successfully") # si tu as beaucoup de sessions cela va être très verbeux
         except grpc._channel._InactiveRpcError as error:
             print(f"Error for canceling session {session_id}: {error.details()}")
 
 
-def create_task_filter(session_id: str, all: bool , creating: bool, error: bool) -> Filter:
+def create_task_filter(session_id: str, all: bool, creating: bool, error: bool) -> Filter:
     """
-    Create a task Filter based on the provided options
+    Create a task Filter for a specific session based on the provided options
 
     Args:
         session_id (str): Session ID to filter tasks
@@ -106,15 +112,18 @@ def list_tasks(client: ArmoniKTasks, task_filter: Filter):
 
     page = 0
     tasks = client.list_tasks(task_filter, page=page)
-    while len(tasks[1]) > 0:
+    while tasks[0]:
         for task in tasks[1]:
-            print(f'Task ID: {task.id}')
+            print(f'Task ID: {task.id}')   # ouh la ca peut defiler...
         page += 1
         tasks = client.list_tasks(task_filter, page=page)
 
+    # comme prrécedemment je pense que le nombre de taches affichés n'est pas correct...)
     print(f"\nTotal tasks: {tasks[0]}\n")
+    # cette fonction fait exactement la meme chose que celle des sessions. Ne serait il pas possible de mutualiser le code?
 
-def check_task(client: ArmoniKTasks, task_ids: list):
+
+def check_task(client: ArmoniKTasks, task_ids: list): # le nom de la fonction n'est pas bon et la description de la fonction ne fait pas ce qu'elle dit qu'elle fait.
     """
     Check the status of a task based on its ID.
 
@@ -124,7 +133,7 @@ def check_task(client: ArmoniKTasks, task_ids: list):
     """
     for task_id in task_ids:
         tasks = client.list_tasks(TaskFieldFilter.TASK_ID == task_id)
-        if len(tasks[1]) > 0:
+        if tasks[0]:
             print(f"\nTask information for task ID {task_id} :\n")
             print(tasks[1])
         else:
